bfa6add09c50cb188b172295ff67b8db2a36af63
josemcorderoc <josemcorderoc@gmail.com>
2020-08-21 12:18:32 -0400

    add example

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # docker-pyspark
-Ready to use Dockefile with Spark (standalone cluster) and Python. 
+Ready to use Dockefile with Spark (standalone cluster) and Python. Check the usage example for more info.
 Forked from https://gist.github.com/szczeles/83bfd283e467957fb617efad1a8d5b55
-
 
--- example/example_app.py
+++ example/example_app.py
@@ -1,5 +1,9 @@
 import logging
 from pyspark.sql import SparkSession
+from pyspark.sql.pandas.functions import pandas_udf
+from pyspark.sql.types import StringType
+from pyspark.sql.functions import col
+import pandas as pd
 
 logging.basicConfig(format='[%(asctime)s] - %(name)s - %(levelname)s : %(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -10,3 +14,15 @@ spark.sparkContext.setLogLevel("ERROR")
 log.info(f"Spark version = {spark.version}")
 log.info(f"Hadoop version = {spark.sparkContext._jvm.org.apache.hadoop.util.VersionInfo.getVersion()}")
 
+log.info('Spark processing example')
+
+
+@pandas_udf(StringType())
+def custom_processing(s: pd.Series) -> pd.Series:
+    return s.apply(lambda x: f"This value is {x}")
+
+
+df = spark.read.format('csv').options(header=True).load('example_data.csv')  # It could be an S3a path
+df.show()
+df = df.select('*', custom_processing(col('column1')).alias('new_column'))
+df.show()
